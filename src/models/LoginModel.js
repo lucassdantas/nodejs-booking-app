e7948eae7bfac14355a8a5c23260a7b8e4d42450
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-07-15 10:56:24 -0300

    validating forms

---
+++ src/models/LoginModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const LoginSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type:String, required: true}
+});
+
+const LoginModel = mongoose.model('Home', LoginSchema);
+
+class Login {
+  constructor(body){
+    this.body = body
+    this.errors = []
+    this.user = null
+  }
+  register(){
+    this.valida()
+  }
+  valida(){
+    this.cleanUp()
+    if(!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido')
+    if(!this.body.password.length < 3 || this.body.password.length > 50){
+      this.errors.push('A senha precisa ter entre 3 e 50 caracteres')
+    }
+    
+  }
+
+  cleanUp(){
+    for(const key in this.body){
+      if( typeof this.body[key] !== 'string'){
+        this.body[key] = ''
+      }
+    }
+
+    this.body = {
+      email: this.body.email,
+      password: this.body.password
+    }
+
+  }
+}
+
+module.exports = Login;
