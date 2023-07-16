6314374f0fbb95a2e4e6e50f02560a6c58d70dd7
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-07-16 10:40:06 -0300

    fix error message. need to fix sucess layour

--- routes.js
+++ routes.js
@@ -8,6 +8,7 @@ route.get('/', homeController.index);
 route.get('/login/',  loginController.index)
 route.post('/login/register',  loginController.register)
 route.post('/login/login',  loginController.login)
+route.get('/login/logout',  loginController.logout)
 
 
 module.exports = route;
