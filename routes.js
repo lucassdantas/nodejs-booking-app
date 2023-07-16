25a194290a037b69e1ed94a907440ff28a51ce1c
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-07-16 11:01:58 -0300

    creating contact register

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contactController = require('./src/controllers/contactController');
 
 // Rotas da home
 route.get('/', homeController.index);
@@ -9,6 +10,6 @@ route.get('/login/',  loginController.index)
 route.post('/login/register',  loginController.register)
 route.post('/login/login',  loginController.login)
 route.get('/login/logout',  loginController.logout)
-
+route.get('/contato/', contactController.index)
 
 module.exports = route;
